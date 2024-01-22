f378b07e06fb43a0bd862fc6c0739c3c9035d7ba
Marius Obert <obatzer@gmail.com>
2024-01-22 22:46:11 +0100

    add kontakt

--- src/app/impressum/page.tsx
+++ src/app/impressum/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function Impressum() {
   return (
     <div className="max-w-xs md:max-w-xl mx-auto">
       <div className="space-y-6">
         <h1 className="text-2xl mb-12">Impressum</h1>
+        <p className="text-xs text-gray-400">Angaben gemäß § 5 TMG</p>
         <p className="">
           Holzhandlung Heidi Obert <br />
           Rankach 30 <br />
@@ -10,13 +13,26 @@ export default function Impressum() {
         </p>
 
         <p className="">
-          Tel.: 0 78 34 / 86 96 40 <br />
-          Fax.: 0 78 34 / 90 71 <br />
-          Email: info@holzhandlung-obert.de <br />
+          Tel: 0 78 34 / 86 96 40 <br />
+          Fax: 0 78 34 / 90 71 <br />
+          Email:{" "}
+          <Link
+            href="mailto:info@holzhandlung-obert.de"
+            className="hover:underline underline-offset-4"
+          >
+            info@holzhandlung-obert.de
+          </Link>
         </p>
 
+        <p className="">Ust.-IdNr.: DE 156642409</p>
+
+        <p className="text-xs text-gray-400">
+          Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:
+        </p>
         <p className="">
-          Ust.-IdNr.: DE 156642409
+          Holzhandlung Heidi Obert <br />
+          Rankach 30 <br />
+          77709 Oberwolfach - Walke
         </p>
       </div>
     </div>
